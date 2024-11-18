93240989e06ba3178b07a8fc79b0ae0dab7d7cf3
AdityaRHavaldar <114271273+AdityaRHavaldar@users.noreply.github.com>
2024-11-18 17:01:51 +0530

    Add files via upload

---
+++ 18-11-24/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import CustomerList from "./Components/Assignment 2/CustomerDetails";
+import LayoutEffect from "./Components/Assignment 3/LayoutEffect";
+import CallBack from "./Components/Assignment 4/CallBack";
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutEffect />
+      <hr />
+      <CustomerList />
+      <hr />
+      <CallBack />
+    </div>
+  );
+}
+
+export default App;
