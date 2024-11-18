f17eeb77895401bbf44080db9b594aed15850522
AdityaRHavaldar <114271273+AdityaRHavaldar@users.noreply.github.com>
2024-11-18 17:13:51 +0530

    Add files via upload

--- 18-11-24/Components/Assignment 3/LayoutEffect.tsx
+++ 18-11-24/Components/Assignment 3/LayoutEffect.tsx
@@ -1,27 +1,26 @@
 // 3.  Create a react component to apply fading effect to heading tag using useLayoutEffect()  hook.
 
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useState, useRef } from "react";
 
 const LayoutEffect: React.FC = () => {
+  const [opacity, setOpacity] = useState(0);
   const headingRef = useRef<HTMLHeadingElement>(null);
 
   useLayoutEffect(() => {
     const headingElement = headingRef.current;
     if (headingElement) {
-      headingElement.style.opacity = "0";
+      setOpacity(1);
 
-      void headingElement.offsetHeight;
-
-      headingElement.style.transition = "opacity 3s ease-in";
-
-      headingElement.style.opacity = "1";
+      setTimeout(() => {
+        headingElement.style.transition = "opacity 3s ease-in";
+      }, 0);
     }
   }, []);
 
   return (
     <div>
       <label>Assignment 3</label>
-      <h1 ref={headingRef}>
+      <h1 ref={headingRef} style={{ opacity }}>
         This Heading has Fading Effect, Will appear in 3 sec's
       </h1>
     </div>
